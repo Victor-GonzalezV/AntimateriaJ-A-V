6e7a990c1d38b9f1cb8c2cc6bfcf5e09ebdebe86
Emilio <Emilito@gamil.com>
2024-11-14 12:31:06 -0400

    commit final Hernandez Gonzalez Lopez

---
+++ partipantes.js
@@ -0,0 +1,4 @@
+  console.log("nombre: 'Victor', apellido: 'Gonzalez', seccion: 'A' ");
+  console.log("nombre: 'Andres', apellido: 'Hernandez', seccion: 'A' ");
+  console.log("nombre: 'Javier', apellido: 'López', seccion: 'A' ");
+  
